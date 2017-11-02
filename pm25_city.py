c233c22d5c0d32ee76886c71fa2acc43b68585bb
tonyxufaker <xutao759153@126.com>
2017-11-02 16:37:50 +0800

    1.增加定时任务，每1小时自动爬取一次 2.每次爬取数据前先爬取城市名称拼音

--- pm25_city.py
+++ pm25_city.py
@@ -5,19 +5,25 @@
 
 from bs4 import BeautifulSoup
 import requests
-from time import ctime
-import threading
 import sqlite3
+from rank_city import city_list
 
 
 
 def getPM25(cityname):
+    #定义网站地址构成
     url = 'http://www.pm25.com/city/'+cityname+'.html'
+    #发起网页请求
     html = requests.get(url)
+    #用Beautifulsoup分析网页，获取网页内容
     soup = BeautifulSoup(html.text, 'html.parser')
+    #解析城市中文名称
     city = soup.select('.city_name')[0].text
-    updated_time = soup.select('.citydata_updatetime')[0].text
+    #解析更新时间，因为爬取到的数据中含有更新时间字符，所以用replace替换掉
+    updated_time = soup.select('.citydata_updatetime')[0].text.replace('更新时间：', '')
+    #解析AQI指数
     AQI = soup.select('.cbol_aqi_num')[0].text
+    #根据AQI指数，判断空气质量等级
     AQI_num = int(AQI)
     if AQI_num >= 50:
         if AQI_num >=100:
@@ -38,39 +44,41 @@ def getPM25(cityname):
             level = '良'
     else:
         level = '优'
+    #解析污染物
     wuranwu = soup.select('.cbol_wuranwu_num ')[0].text
+    #解析PM2.5
     PM25 = soup.select('.cbol_nongdu_num_1')[0].text
+    #解析单位
     unit = soup.select('.cbol_nongdu_num_2')[0].text
-    print(city+'\n'+updated_time+'\n'+'AQI指数：'+AQI+'\n'+"首要污染物："+wuranwu+'\n'+'PM25：'+PM25+unit+'\n'+'空气质量等级：'+level+'\n'+'查询时间：'+ctime())
 
-    #连接数据库，如果数据库不存在，则创建一个
-    conn = sqlite3.connect('E:\pyproject\PM25\db.sqlite3\main.db')
+
+
+    # 定义数据库路径
+    db = r'E:\\pyproject\PM25\db.sqlite3'
+    # 连接数据库，如果数据库不存在，则创建一个
+    conn = sqlite3.connect(db)
     cusor = conn.cursor()
-    cusor.execute("INSERT INTO PM_City (city, AQI, wuranwu, PM25, unit, level, updated_time) VALUES ('%s', %d, '%s', %d, '%s', '%s', '%s')")
+    #执行数据库操作：插入数据
+    cusor.execute("SELECT * FROM PM_CITY WHERE (city='%s' AND updated_time='%s')"% (city, updated_time))
+    result = cusor.fetchall()
+    if len(result)>0:
+        print("数据 %s %s 已存在" % (city, updated_time) )
+        return
+    else:
+        cusor.execute("INSERT INTO PM_City (city, AQI, wuranwu, PM25, unit, level, updated_time) VALUES ('%s', %d, '%s', %d, '%s', '%s', '%s')" % (city, int(AQI), wuranwu, int(PM25), unit, level, updated_time))
+        print("数据 %s %s插入成功！" % (city, updated_time))
+    #提交数据库操作
     conn.commit()
+    #关闭数据库链接
     conn.close()
 
 
+#根据城市列表爬取各城市的PM2.5数据
 def one_thread():
-    cn = input('请输入城市名称拼音：')
-    getPM25(cn)
+    for cityname in city_list:
+        getPM25(cityname)
 
-def two_thread():
-    threads = []
-    cn1 = input('请输入城市名称拼音：')
-    cn2 = input('请输入城市名称拼音：')
-    t1 = threading.Thread(target=getPM25, args=cn1)
-    threads.append(t1)
-    t2 = threading.Thread(target=getPM25, args=cn2)
-    threads.append(t2)
 
-    for t in threads:
-        t.start()
-
-if __name__ == '__main__':
-    one_thread()
-    print('\n\n')
-    two_thread()
 
 
 
---
+++ time_task.py
@@ -0,0 +1,32 @@
+import os, datetime, platform
+import pm25_city
+import rank_city
+
+def run_Task():
+    print('开始爬取...'+'\n'+'读取城市列表...')
+    rank_city.getCity()
+    pm25_city.one_thread()
+    print('爬取结束...')
+
+def timerFun(sched_Timer):
+    while True:
+        now = datetime.datetime.now()
+        if sched_Timer <= now < (sched_Timer + datetime.timedelta(hours=1)):
+            run_Task()
+            sched_Timer += datetime.timedelta(hours=1)
+        elif now >= (sched_Timer + datetime.timedelta(hours=1)):
+            sched_Timer += datetime.timedelta(hours=1)
+        '''
+                elif num >= 3600:
+            if (sched_Timer-now).total_seconds() > 0:
+                pass
+            else:
+                sched_Timer += datetime.timedelta(hours=1)
+        '''
+
+
+
+if __name__ == '__main__':
+    sched_Timer = datetime.datetime.now()
+    print("定时爬取任务已开始，当前时间为 %s" % sched_Timer)
+    timerFun(sched_Timer)
